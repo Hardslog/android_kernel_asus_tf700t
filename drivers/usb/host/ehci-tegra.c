b5deb73cbd103644f239e2bd584b951683464dad
Preetham Chandru R <pchandru@nvidia.com>
2012-11-29 17:08:50 +0530

    usb: tegra: conditionally set wake enable
    Set wake enable only if remote wakeup is supported
    by the platform
    
    Bug 1039143
    
    Signed-off-by: Preetham Chandru R <pchandru@nvidia.com>
    Change-Id: I805dd773cdbb5639f59b2f5ba4bcb2e14be74d5b
    Reviewed-on: http://git-master/r/147462
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: Matthew Pedro <mapedro@nvidia.com>
    
    Conflicts:
    	drivers/usb/host/ehci-tegra.c

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -509,6 +509,7 @@ static int tegra_ehci_probe(struct platform_device *pdev)
 	struct resource *res;
 	struct usb_hcd *hcd;
 	struct tegra_ehci_hcd *tegra;
+	struct tegra_usb_platform_data *pdata = dev_get_platdata(&pdev->dev);
 	int err = 0;
 	int irq;
 
@@ -603,14 +604,16 @@ static int tegra_ehci_probe(struct platform_device *pdev)
 		goto fail_phy;
 	}
 
-	/*err = enable_irq_wake(tegra->irq);
-	if (err < 0) {
-		dev_warn(&pdev->dev,
-				"Couldn't enable USB host mode wakeup, irq=%d, "
-				"error=%d\n", irq, err);
-		err = 0;
-		tegra->irq = 0;
-	}*/
+	if (pdata->u_data.host.remote_wakeup_supported) {
+		err = enable_irq_wake(tegra->irq);
+		if (err < 0) {
+			dev_warn(&pdev->dev,
+					"Couldn't enable USB host mode wakeup,"
+					" irq=%d error=%d\n", irq, err);
+			err = 0;
+			tegra->irq = 0;
+		}
+	}
 
 	tegra->ehci = hcd_to_ehci(hcd);
 
@@ -685,6 +688,7 @@ static int tegra_ehci_suspend(struct platform_device *pdev, pm_message_t state)
 static int tegra_ehci_remove(struct platform_device *pdev)
 {
 	struct tegra_ehci_hcd *tegra = platform_get_drvdata(pdev);
+	struct tegra_usb_platform_data *pdata = dev_get_platdata(&pdev->dev);
 	struct usb_hcd *hcd = ehci_to_hcd(tegra->ehci);
 
 	if (tegra == NULL || hcd == NULL)
@@ -697,15 +701,8 @@ static int tegra_ehci_remove(struct platform_device *pdev)
 	}
 #endif
 
-	if (tegra->phy->inst == 2 && usb3_init == 1) {
-		free_irq(gpio_dock_in_irq, hcd);
-		usb3_ehci_handle = NULL;
-		usb3_init = 0;
-		dock_port_device = NULL;
-	}
-
-	//if (tegra->irq)
-		//disable_irq_wake(tegra->irq);
+	if (tegra->irq && pdata->u_data.host.remote_wakeup_supported)
+		disable_irq_wake(tegra->irq);
 
 	/* Make sure phy is powered ON to access USB register */
 	if(!tegra_usb_phy_hw_accessible(tegra->phy))
